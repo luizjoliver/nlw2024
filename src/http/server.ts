bec07972e4cb33583797a272fa7cadfa4489460b
luizfellipejoliver14 <luizfellipejoliver14@gmail.com>
2024-02-08 20:13:36 -0300

    primeiro dia

---
+++ src/http/server.ts
@@ -0,0 +1,41 @@
+import fastify from "fastify"
+import {PrismaClient} from "@prisma/client"
+import {z} from "zod"
+
+
+const app = fastify()
+
+
+const prisma = new PrismaClient()
+
+app.post("/polls",async (req,reply) =>{
+
+    const createPollBody = z.object({
+        title:z.string()
+    })
+    
+  const {title} =  createPollBody.parse(req.body)
+
+    try {
+
+       const poll =  await prisma.poll.create({
+            data:{
+                title
+            }
+        })
+
+        return reply.status(201).send({pollId:poll.id})
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+
+
+})
+
+app.listen({port:3333}).then(() =>{
+    console.log("http server running");
+    
+})
